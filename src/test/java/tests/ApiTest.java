25b6d3a623a967cae28fd8c8297aa9b4b8435737
Poojashri <poojashri465@gmail.com>
2026-07-13 22:39:41 +0530

    API test updated

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -5,16 +5,16 @@ import com.microsoft.playwright.APIResponse;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ApiHelper;
+import utils.RandomDataUtil;
 
 public class ApiTest extends BaseTest {
 
-    @Test
-    public void verifyAudienceEstimateAPI() {
+    @Test(priority = 1)
+    public void verifyAudienceEstimateApi() {
 
         ApiHelper api = new ApiHelper(playwright);
 
-        APIResponse response =
-                api.estimateAudience("trial-users", "Email");
+        APIResponse response = api.estimateAudience("trial-users", "Email");
 
         Assert.assertEquals(response.status(), 200);
 
@@ -23,8 +23,43 @@ public class ApiTest extends BaseTest {
         System.out.println(body);
 
         Assert.assertTrue(body.contains("\"estimatedAudience\":1840"));
-        Assert.assertTrue(body.contains("\"trial-users\""));
         Assert.assertTrue(body.contains("\"Email\""));
+        Assert.assertTrue(body.contains("\"SMS\""));
+        Assert.assertTrue(body.contains("\"Push\""));
+
+    }
+
+    @Test(priority = 2)
+    public void verifyCreateCampaignApi() {
+
+        ApiHelper api = new ApiHelper(playwright);
+
+        String campaign = RandomDataUtil.campaignName();
+
+        String body = """
+                {
+                  "name":"%s",
+                  "channel":"Email",
+                  "audienceSegment":"trial-users",
+                  "sendMode":"now",
+                  "scheduledAt":"",
+                  "message":"This is an API automation campaign."
+                }
+                """.formatted(campaign);
+
+        APIResponse response = api.createCampaign(body);
+
+        Assert.assertEquals(response.status(), 201);
+
+        String responseBody = response.text();
+
+        System.out.println(responseBody);
+
+        Assert.assertTrue(responseBody.contains(campaign));
+        Assert.assertTrue(responseBody.contains("\"channel\":\"Email\""));
+        Assert.assertTrue(responseBody.contains("\"audienceSegment\":\"trial-users\""));
+        Assert.assertTrue(responseBody.contains("\"status\":\"Draft\""));
+        Assert.assertTrue(responseBody.contains("\"estimatedAudience\":1840"));
 
     }
 
